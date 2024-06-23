20713e7570c49c5f7fce602a0e0c14efa30b1367
decenchat <contact@decenchat.com>
2024-06-23 22:59:44 +0530

    fix iframe style

--- src/widget/index.js
+++ src/widget/index.js
@@ -1,12 +1,15 @@
 !(function() {
-  var i = document.createElement('iframe');
+  var d = document;
+  var i = d.createElement('iframe');
   i.id = 'decentralised-chat';
   i.style.position = 'fixed';
   i.style.bottom = '20px';
   i.style.right = '20px';
-  i.style.width = '100dvw';
-  i.style.height = '100dvh';
+  i.style.width = '400px';
+  i.style.height = '600px';
   i.style.border = 'none';
+  i.style.backgroundColor = 'transparent';
+  i.style.zIndex = '1000';
   d.body.appendChild(i);
 
   i.contentWindow.document.open("text/html", "replace");
